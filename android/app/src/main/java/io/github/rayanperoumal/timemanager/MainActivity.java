ca9c2580f212fe9de97e4700042c68df6265f634
rayanperoumal <rayan.peroumal@gmail.com>
2017-11-20 08:42:15 -0400

    Timer class improve

--- android/app/src/main/java/io/github/rayanperoumal/timemanager/MainActivity.java
+++ android/app/src/main/java/io/github/rayanperoumal/timemanager/MainActivity.java
@@ -2,12 +2,26 @@ package io.github.rayanperoumal.timemanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView minute, second;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        minute = (TextView) findViewById(R.id.minute);
+        second = (TextView) findViewById(R.id.second);
+        Timer timer = new Timer(40);
+        timer.start();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+
 }
